e3aaabdb9ca0ed76b71709ca1397bff7bba5c4c0
Philip Ardley <philard@gmail.com>
2017-03-01 23:22:10 +0800

    * Integration test and Units tests now share the same test data. * Made a big efforts to build a mocked fxRatesService for unit tests.
    Unit tests working now for test data with no null currencies.

--- src/testsrc/java/com/epam/jamp2/service/ITCalculationCommandExecutionService.java
+++ src/testsrc/java/com/epam/jamp2/service/ITCalculationCommandExecutionService.java
@@ -13,20 +13,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContextManager;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Collection;
 
 import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.number.BigDecimalCloseTo.closeTo;
 import static org.junit.Assert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 @RunWith(Parameterized.class)
 //@RunWith(SpringJUnit4ClassRunner.class)
@@ -41,7 +38,7 @@ public class ITCalculationCommandExecutionService {
         testContextManager = new TestContextManager(ITCalculationCommandExecutionService.class);
     }
     @Before
-    public void setUpStringContextt() throws Exception {
+    public void injectDependencies() throws Exception {
         testContextManager.prepareTestInstance(this);
     }
 
@@ -51,31 +48,10 @@ public class ITCalculationCommandExecutionService {
     private CalculationCommand command;
     private Value expected;
 
-    // Parameterized data for tests
+    // Parameterized data for integration tests
     @Parameters(name = "{index}: test calculate (command: {0}, {1}, {2}, {3}) should return = {4}")
     public static Collection<Object[]> data() {
-
-        return Arrays.asList(new Object[][]{
-                {new Value("gbp", BigDecimal.valueOf(1.0)), new Value("gbp", BigDecimal.valueOf(2.0))
-                        , Operation.ADD, "gbp", new Value("gbp", BigDecimal.valueOf(3.0))},
-                {new Value("gbp", BigDecimal.valueOf(3.0)), new Value("gbp", BigDecimal.valueOf(2.0))
-                        , Operation.SUBTRACT, "gbp", new Value("gbp", BigDecimal.valueOf(1.0))},
-                {new Value("gbp", BigDecimal.valueOf(1.0)), new Value("cny", BigDecimal.valueOf(2.0))
-                        , Operation.ADD, "cny", new Value("cny", BigDecimal.valueOf(12.0))},
-                {new Value("gbp", BigDecimal.valueOf(10000000000000000001.0)),
-                        new Value("cny", BigDecimal.valueOf(20000000000000000000.0))
-                        , Operation.ADD, "cny", new Value("cny", BigDecimal.valueOf(120000000000000000000.0))},
-                {new Value("gbp", BigDecimal.valueOf(0.00001)), new Value("gbp", BigDecimal.valueOf(0.0))
-                        , Operation.ADD, "cny", new Value("cny", BigDecimal.valueOf(0.0001))},
-                {new Value("gbp", BigDecimal.valueOf(1.0)), new Value("gbp", BigDecimal.valueOf(2.0))
-                        , Operation.ADD, null, new Value("gbp", BigDecimal.valueOf(3.0))},
-                {new Value("gbp", BigDecimal.valueOf(1.0)), new Value("cny", BigDecimal.valueOf(2.0))
-                        , Operation.ADD, null, new Value("gbp", BigDecimal.valueOf(1.2))},
-                {new Value("gbp", BigDecimal.valueOf(1.0)), new Value(null, BigDecimal.valueOf(2.0))
-                        , Operation.ADD, null, new Value("gbp", BigDecimal.valueOf(3.0))},
-                {new Value(null, BigDecimal.valueOf(1.0)), new Value(null, BigDecimal.valueOf(2.0))
-                        , Operation.ADD, "gbp", new Value("gbp", BigDecimal.valueOf(3.0))}
-        });
+        return TestCalculationCommandExecutionService.data();
     }
 
     // constructor
@@ -88,7 +64,7 @@ public class ITCalculationCommandExecutionService {
     @Test
     public void test_calculate() throws IOException, UnknownCurrencyException {
         Value result = target.calculate(command);
-        BigDecimal errorTolerance = expected.getValue().multiply(BigDecimal.valueOf(0.15)); // i.e. ±15% for currency fluctuations
+        BigDecimal errorTolerance = expected.getValue().multiply(BigDecimal.valueOf(0.20)); // i.e. ±15% for currency fluctuations
         assertThat((result), hasProperty("value", closeTo(expected.getValue(), errorTolerance)));
         assertThat(result.getCurrencyCode(), equalTo(expected.getCurrencyCode()));
     }
